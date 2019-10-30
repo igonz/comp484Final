7baef85e4f9d77bad6bd61116225395904a253e4
IsmaelG <ismaelg@netflix.com>
2019-10-29 20:43:47 -0700

    Added Drawing POC

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log(msg);
     io.emit('chat message', msg);
   });
+
+  socket.on('drawingCoords', function(msg){
+    console.log(msg);
+  })
 });
 
 http.listen(3000, function(){
