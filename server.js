9a7f34a68e1241cc159d3dc3130e229fffad6dfb
IsmaelG <ismaelg@netflix.com>
2019-11-05 20:15:12 -0800

    Separated css, js to public folder

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+var connectedUsersToColors = {};
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  socket.send(socket.id);
+
   socket.on('chat message', function(msg){
     console.log(msg);
     io.emit('chat message', msg);
   });
 
   socket.on('drawingCoords', function(msg){
+    console.log(msg);
+  });
+
+  socket.on('disconnect', function(msg) {
+    console.log('Disconnected');
     console.log(msg);
   })
 });
